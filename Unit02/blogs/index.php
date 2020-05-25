85da7767a952cbc04d8f31bee832620b33ebe004
buiminh1995 <buiminh2000@gmail.com>
2020-05-25 19:40:33 +0700

    Load 7 bài viết

--- Unit02/blogs/index.php
+++ Unit02/blogs/index.php
@@ -25,6 +25,18 @@ while($row = $result_recent_posts->fetch_assoc()){
 	$recent_posts[] = $row;
 }
 
+$query_post_col_12 = "SELECT * FROM posts ORDER BY created_at desc limit 6,7";
+
+// Thuc thi cau lenh
+$result_col_12 = $conn->query($query_post_col_12 );
+
+// Tao 1 mang de chua du lieu
+$post_col_12 = array();
+
+while($row = $result_col_12->fetch_assoc()){
+	$post_col_12[] = $row;
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -224,15 +236,12 @@ while($row = $result_recent_posts->fetch_assoc()){
 					<div class="clearfix visible-md visible-lg"></div>
 
 					<!-- post -->
-					
 					<!-- /post -->
 
 					<!-- post -->
-					
 					<!-- /post -->
 
 					<!-- post -->
-					
 					<!-- /post -->
 				</div>
 				<!-- /row -->
@@ -242,112 +251,65 @@ while($row = $result_recent_posts->fetch_assoc()){
 					<div class="col-md-8">
 						<div class="row">
 							<!-- post -->
+							<?php
+							foreach($post_col_12 as $post){
+							?>	
 							<div class="col-md-12">
 								<div class="post post-thumb">
-									<a class="post-img" href="blog-post.html"><img src="./img/post-2.jpg" alt=""></a>
+									<a class="post-img" href="blog-post.html"><img src="<?php echo $post['thumbnail'] ?>" alt="" ></a>
 									<div class="post-body">
 										<div class="post-meta">
 											<a class="post-category cat-3" href="category.html">Jquery</a>
-											<span class="post-date">March 27, 2018</span>
+											<span class="post-date"><?php echo $post['created_at'] ?></span>
 										</div>
-										<h3 class="post-title"><a href="blog-post.html">Ask HN: Does Anybody Still Use JQuery?</a></h3>
+										<h3 class="post-title"><a href="blog-post.html"><?php echo $post['title'] ?></a></h3>
 									</div>
 								</div>
 							</div>
+							<?php } ?>
 							<!-- /post -->
 
 							<!-- post -->
+							<?php
+							foreach($recent_posts as $post){
+							?>	
 							<div class="col-md-6">
 								<div class="post">
-									<a class="post-img" href="blog-post.html"><img src="./img/post-1.jpg" alt=""></a>
+									<a class="post-img" href="blog-post.html"><img src="<?php echo $post['thumbnail'] ?>" alt="" width="400px" height="300px"></a>
 									<div class="post-body">
 										<div class="post-meta">
 											<a class="post-category cat-4" href="category.html">Css</a>
-											<span class="post-date">March 27, 2018</span>
+											<span class="post-date"><?php echo $post['created_at'] ?></span>
 										</div>
-										<h3 class="post-title"><a href="blog-post.html">CSS Float: A Tutorial</a></h3>
+										<h3 class="post-title"><a href="blog-post.html"><?php echo $post['title'] ?></a></h3>
 									</div>
 								</div>
 							</div>
+							<?php } ?>
 							<!-- /post -->
 
 							<!-- post -->
-							<div class="col-md-6">
-								<div class="post">
-									<a class="post-img" href="blog-post.html"><img src="./img/post-2.jpg" alt=""></a>
-									<div class="post-body">
-										<div class="post-meta">
-											<a class="post-category cat-1" href="category.html">Web Design</a>
-											<span class="post-date">March 27, 2018</span>
-										</div>
-										<h3 class="post-title"><a href="blog-post.html">Tell-A-Tool: Guide To Web Design And Development Tools</a></h3>
-									</div>
-								</div>
-							</div>
+							
 							<!-- /post -->
 
 							<div class="clearfix visible-md visible-lg"></div>
 
 							<!-- post -->
-							<div class="col-md-6">
-								<div class="post">
-									<a class="post-img" href="blog-post.html"><img src="./img/post-4.jpg" alt=""></a>
-									<div class="post-body">
-										<div class="post-meta">
-											<a class="post-category cat-2" href="category.html">JavaScript</a>
-											<span class="post-date">March 27, 2018</span>
-										</div>
-										<h3 class="post-title"><a href="blog-post.html">Chrome Extension Protects Against JavaScript-Based CPU Side-Channel Attacks</a></h3>
-									</div>
-								</div>
-							</div>
+							
 							<!-- /post -->
 
 							<!-- post -->
-							<div class="col-md-6">
-								<div class="post">
-									<a class="post-img" href="blog-post.html"><img src="./img/post-5.jpg" alt=""></a>
-									<div class="post-body">
-										<div class="post-meta">
-											<a class="post-category cat-3" href="category.html">Jquery</a>
-											<span class="post-date">March 27, 2018</span>
-										</div>
-										<h3 class="post-title"><a href="blog-post.html">Ask HN: Does Anybody Still Use JQuery?</a></h3>
-									</div>
-								</div>
-							</div>
+							
 							<!-- /post -->
 
 							<div class="clearfix visible-md visible-lg"></div>
 
 							<!-- post -->
-							<div class="col-md-6">
-								<div class="post">
-									<a class="post-img" href="blog-post.html"><img src="./img/post-3.jpg" alt=""></a>
-									<div class="post-body">
-										<div class="post-meta">
-											<a class="post-category cat-1" href="category.html">Web Design</a>
-											<span class="post-date">March 27, 2018</span>
-										</div>
-										<h3 class="post-title"><a href="blog-post.html">Pagedraw UI Builder Turns Your Website Design Mockup Into Code Automatically</a></h3>
-									</div>
-								</div>
-							</div>
+							
 							<!-- /post -->
 
 							<!-- post -->
-							<div class="col-md-6">
-								<div class="post">
-									<a class="post-img" href="blog-post.html"><img src="./img/post-4.jpg" alt=""></a>
-									<div class="post-body">
-										<div class="post-meta">
-											<a class="post-category cat-2" href="category.html">JavaScript</a>
-											<span class="post-date">March 27, 2018</span>
-										</div>
-										<h3 class="post-title"><a href="blog-post.html">Chrome Extension Protects Against JavaScript-Based CPU Side-Channel Attacks</a></h3>
-									</div>
-								</div>
-							</div>
+							
 							<!-- /post -->
 						</div>
 					</div>
